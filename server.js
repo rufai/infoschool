8bbc9ab70df5b2f1148e20d8cd78d01138db6e09
rufai <rufai.mustopha@gmail.com>
2017-11-11 11:15:58 +0100

    added requirement for ejs template engine

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 6000;
+var express = require("express"),
+	app = express(),
+	port = process.env.PORT || 3000,
+	mongoose = require('mongoose'),
+	School = require('./api/model/info_school_model.js'),
+	bodyParser = require('body-parser'),
+	path = require("path");
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/InfoSchooldb');
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/route/info_school_routes');
+routes(app);
+
+
 
 app.listen(port);
 
-console.log('Info School Server ' + port);
+console.log("Test listening port at " + port);
