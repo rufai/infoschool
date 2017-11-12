b1720b6ff0681c6aa5087175fc4bc0cba858c155
rufai <rufai.mustopha@gmail.com>
2017-11-12 14:09:58 +0100

    Preparations for heroku deployment

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 var express = require("express"),
 	app = express(),
 	port = process.env.PORT || 3000,
+	path = require("path"),
 	mongoose = require('mongoose'),
-	School = require('./api/model/info_school_model.js'),
 	bodyParser = require('body-parser'),
-	path = require("path");
+	routes = require('./api/route/info_school_routes'),
+	School = require('./api/model/info_school_model.js');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/InfoSchooldb');
 
+app.use(express.static(__dirname + '/public'));
+
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", "./views/pages/");
 
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/route/info_school_routes');
 routes(app);
-
-
-
 app.listen(port);
 
 console.log("Test listening port at " + port);
